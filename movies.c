4d672c55ffe5112d27612f9cfa9e6bf666628609
josephsathishjo <104755445+josephsathishjo@users.noreply.github.com>
2022-05-14 21:45:35 +0530

    Movies

---
+++ movies.c
@@ -0,0 +1,35 @@
+SELECT movies.title FROM  people
+
+JOIN stars ON people.id=stars.person_id
+
+JOIN movies ON stars.movie_id=movies.id
+
+WHERE people.name="Johnny Depp"AND movies.title IN(
+
+SELECT movies.title FROM  people
+
+JOIN stars ON people.id=stars.person_id
+
+JOIN movies ON stara.movie_id=movies.id
+
+WHERE people.name="Helena Bonham Carter");
+
+//other soluction
+
+SELECT movies.title FROM people
+
+JOIN stars ON people.id=stars.person_id
+
+JOIN movies ON stars.movie_id=movies.if
+
+WHERE people.name="Johnny Depp"
+
+INTERSECT
+
+SELECT movies.title FROM people
+
+JOIN stars ON people.id=stars.person_id
+
+JOIN movies ON stars.movie_id=movies.id
+
+WHERE people.name="Helena Bonham Carter";
